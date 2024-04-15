94f663fc985cd691b7dd67d49060646b923beffc
alejack312 <alejack312@gmail.com>
2024-04-15 11:22:47 -0400

    feat: setup for custom sign-in and sign-up pages

---
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,16 @@
+import { SignIn } from "@clerk/nextjs";
+import { dark, neobrutalism, shadesOfPurple } from "@clerk/themes";
+
+export default function Page() {
+  return (
+    <div className="flex justify-center items-center h-screen w-full">
+      <div className="w-full max-w-md">
+        <SignIn
+          appearance={{
+            baseTheme: neobrutalism,
+          }}
+        />
+      </div>
+    </div>
+  );
+}
