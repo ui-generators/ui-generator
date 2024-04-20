aefae24b3d1b7c285e87d1176747c71d4e22855d
NolanChen11 <nchen@oberlin.edu>
2024-04-20 17:51:25 -0400

    prompt updated with system and user prompt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from 'react';
 import Form, { FormInput } from '../components/form';
-import { generatePrompt } from '../app/prompt';
+import { GetSystemPrompt, GetWebpagePrompt, GetIteractionSystemPrompt } from '../app/prompt';
 import { fetchFromOpenAI } from '../app/fetchFromOpenAI';
 
 
@@ -11,11 +11,6 @@ const Home: React.FC = () => {
   const [blobUrls, setBlobUrls] = useState<string[]>([]);
 
   const handleSubmit = async (formInput: FormInput): Promise<void> => {
-
-    const prompt = generatePrompt(formInput);
-    const generatedCode = await fetchFromOpenAI(prompt);
-    console.log(generatedCode);
-
     // doing simple generation using only some of the user inputs for placeholder purposes
     const baseCode = `<html><body style="background-color:${formInput.colorScheme};"><h1>${formInput.pageTitle}</h1><p>${formInput.content}</p></body></html>`;
     const variations = [
