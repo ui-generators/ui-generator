688628bb0d053ceb16e4a2fc029612be0aee3fa8
alejack312 <alejack312@gmail.com>
2024-04-29 13:48:06 -0400

    feat: custom sign-in and sign-up pages

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,16 +1,21 @@
 import { SignIn } from "@clerk/nextjs";
 import { dark, neobrutalism, shadesOfPurple } from "@clerk/themes";
+import styles from "./styles.module.css";
 
 export default function Page() {
   return (
-    <div className="flex justify-center items-center h-screen w-full">
-      <div className="w-full max-w-md">
-        <SignIn
-          appearance={{
-            baseTheme: neobrutalism,
-          }}
-        />
+    <div className={styles.mainContainer}>
+      <div className={styles.loginForm}>
+        <h1 className={styles.welcomeMessage}>UI Generator</h1>
+        <div className={styles.googleButton}>
+          <SignIn />
+        </div>
+
+        <h1 className={styles.teaseMessage}>
+          Creating interfaces that excite, one prompt at a time.
+        </h1>
       </div>
     </div>
   );
 }
+
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,21 @@
 import { SignUp } from "@clerk/nextjs";
+import styles from "./styles.module.css";
 
 export default function Page() {
-  return <SignUp />;
+
+  return (
+  <div className={styles.mainContainer}>
+      <div className={styles.loginForm}>
+        <h1 className={styles.welcomeMessage}>
+          UI Generator
+        </h1>
+        <div className={styles.googleButton}>
+          <SignUp />
+        </div>
+
+        <h1 className={styles.teaseMessage}>
+          Creating interfaces that excite, one prompt at a time.
+        </h1>
+      </div>
+    </div>);
 }
