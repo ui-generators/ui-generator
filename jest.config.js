5b3ef0e17cfbd24261d83206fe1de6fc66d59dcb
ywan3 <yajie_wan@brown.edu>
2024-05-01 20:09:15 -0400

    initial working test: run with 'npm run unit-tests'

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    testEnvironment: 'jest-environment-jsdom',
+    setupFilesAfterEnv: ['./jest.setup.js'],
+    transform: {
+      '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest', // Transform JS and TypeScript files with babel-jest
+    },
+    moduleNameMapper: {
+      // Handle module aliases and static file imports (if used in your project)
+      '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+      '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+    },
+  };
