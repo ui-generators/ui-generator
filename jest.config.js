4674250338f3e63da30766260a3d24e1c99e0480
BrianEdmund <brian_sutioso@brown.edu>
2024-05-07 00:15:58 -0400

    Temp fix for jest tests

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,19 @@
-module.exports = {
-    testEnvironment: 'jest-environment-jsdom',
-    setupFilesAfterEnv: ['./jest.setup.js'],
-    transform: {
-      '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest', // Transform JS and TypeScript files with babel-jest
-    },
-    moduleNameMapper: {
-      // Handle module aliases and static file imports (if used in your project)
-      '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
-      '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
-    },
-  };
+// Small temp fix
+export default {
+    testEnvironment: 'node',
+    transform: {},
+    extensionsToTreatAsEsm: ['.ts', '.tsx', '.jsx'],
+};
+
+// module.exports = {
+//   testEnvironment: 'jest-environment-jsdom',
+//   setupFilesAfterEnv: ['./jest.setup.js'],
+//   transform: {
+//     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest', // Transform JS and TypeScript files with babel-jest
+//   },
+//   moduleNameMapper: {
+//     // Handle module aliases and static file imports (if used in your project)
+//     '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+//     '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+//   },
+// };
